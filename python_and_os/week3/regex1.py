3ffd25fab7d3703b184fa850bf08f97b7c5f5ae1
lakshyajit165 <lakshyajit165@gmail.com>
2020-03-06 21:21:29 +0530

    continuing with regex

---
+++ python_and_os/week3/regex1.py
@@ -0,0 +1,6 @@
+#!usr/bin/env python3
+import re
+log = "July 31 07:51:48 mycomputer bad_process[12345]: Error performing package update"
+regex = r"\[(\d+)\]"
+result = re.search(regex, log)
+print(result[1])
---
+++ python_and_os/week3/regex2.py
@@ -0,0 +1,14 @@
+# code to check if the text passed contains punctuation symbols: commas, periods, colons, semicolons, question marks, and exclamation points.
+
+#!usr/bin/env python3
+
+import re
+def check_punctuation(text):
+  result = re.search(r"[,.:;?!]", text)
+  return result != None
+
+print(check_punctuation("This is a sentence that ends with a period.")) # True
+print(check_punctuation("This is a sentence fragment without a period")) # False
+print(check_punctuation("Aren't regular expressions awesome?")) # True
+print(check_punctuation("Wow! We're really picking up some steam now!")) # True
+print(check_punctuation("End of the line")) # False
