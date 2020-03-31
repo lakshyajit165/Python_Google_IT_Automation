fed4632b75ee44b616a778ff357b00573e5dcf29
Lakshyajit <lakshyajit165@gmail.com>
2020-03-31 13:56:49 +0530

    week 4 complete

---
+++ python_and_os/week4/check_cron.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import re
+import sys
+
+logfile = sys.argv[1]
+usernames = {}
+
+
+with open(logfile) as f:
+    for line in f:
+        if "CRON" not in line:
+            continue
+        pattern = r"USER \((\w+)\)$"
+        result = re.search(pattern, line)
+        
+        if result is None:
+            continue
+
+        name = result[1]
+        usernames[name] = usernames.get(name, 0) + 1
+
+print(usernames)
