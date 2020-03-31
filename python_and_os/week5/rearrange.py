857f9cffca6644063b9f56d327f48e92319595cc
Lakshyajit <lakshyajit165@gmail.com>
2020-03-31 23:56:52 +0530

    week 5 started, unittesting

---
+++ python_and_os/week5/rearrange.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import re
+
+def rearrange_name(name):
+    result = re.search(r"^([\w .]*), ([\w .]*)$", name)
+    if result is None:
+        return name
+    return "{} {}".format(result[2], result[1])
+
---
+++ python_and_os/week5/rearrange_test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from rearrange import rearrange_name
+import unittest
+
+class TestRearrange(unittest.TestCase):
+    def test_basic(self):
+        testcase = "Lovelace, Ada"
+        expected = "Ada Lovelace"
+        self.assertEqual(rearrange_name(testcase), expected)
+
+    def test_empty(self):
+        testcase = ""
+        expected = ""
+        self.assertEqual(rearrange_name(testcase), expected)
+
+    def test_double_name(self):
+        testcase = "Hopper, Grace M."
+        expected = "Grace M. Hopper"
+        self.assertEqual(rearrange_name(testcase), expected)
+
+    def test_one_name(self):
+        testcase = "Voltaire"
+        expected = "Voltaire"
+        self.assertEqual(rearrange_name(testcase), expected)
+
+unittest.main()
