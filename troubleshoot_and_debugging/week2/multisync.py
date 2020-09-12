71568907307c1305183c3caf187cd7cceaad5f66
Vaibhav Gulati <gulvaibhav20@gmail.com>
2020-09-13 01:24:26 +0530

    Updated the multisync.py file Added a "/" in the home_add variable in order to assign proper addresses (PATH) to src and dest variables.

--- troubleshoot_and_debugging/week2/multisync.py
+++ troubleshoot_and_debugging/week2/multisync.py
@@ -3,9 +3,13 @@ from multiprocessing import Pool
 import multiprocessing
 import subprocess
 import os
-home_add = os.path.expanduser('~')      #Gets home path
-src = home_add + "data/prod/"
-dest = home_add + "data/prod_backup/"
+
+home_add = os.path.expanduser('~')      # Gets home path as /home/username
+
+home_add = home_add + "/"               # Added a "/" to change value stored in home_add (/home/username) to (/home/username/)
+
+src = home_add + "data/prod/"           # It points to /home/username/data/prod/
+dest = home_add + "data/prod_backup/"   # It points to /home/username/data/prod_backup/
 
 if __name__ == "__main__":
   pool = Pool(multiprocessing.cpu_count())
